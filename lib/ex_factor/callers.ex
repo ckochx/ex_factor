bab3323afa41db6d6d8a60cb561f5f8c7ffbdf5a
Christian Koch <ckoch@cars.com>
2021-09-24 08:31:36 -0500

    update callers to add callers/3 fn.
    Use mod, fn, arity to match.

--- lib/ex_factor/callers.ex
+++ lib/ex_factor/callers.ex
@@ -18,6 +18,32 @@ defmodule ExFactor.Callers do
     |> mangle_list()
   end
 
+  def callers(mod, func, arity) do
+    Mix.Tasks.Xref.calls([])
+    |> Enum.filter(fn x ->
+      x.callee == {cast(mod), cast(func), arity}
+    end)
+  end
+
+  def cast(value) when is_atom(value), do: value
+  def cast(value) do
+    cond do
+      String.downcase(value) == value -> String.to_atom(value)
+      true ->
+        cast_module(value)
+    end
+  end
+
+  def cast_module(module) do
+    if String.match?(module, ~r/Elixir\./) do
+      module
+    else
+      "Elixir." <> module
+    end
+    |> Module.split()
+    |> Module.concat()
+  end
+
   defp mangle_list([""]), do: []
   defp mangle_list(["Compiling" <> _ | tail]), do: mangle_list(tail)
 
--- test/ex_factor/callers_test.exs
+++ test/ex_factor/callers_test.exs
@@ -15,4 +15,34 @@ defmodule ExFactor.CallersTest do
       assert [] = Callers.callers(ExFactor.NotAModule)
     end
   end
+
+  describe "callers/3" do
+    test "it should report callers of a module" do
+      [one, _two, _three] = Callers.callers(ExFactor.Parser, :all_functions, 1)
+
+      assert one.caller_module == ExFactor.Callers
+      assert one.file == "lib/ex_factor/callers.ex"
+      assert one.line == 9
+    end
+
+    test "it converts strings to atoms" do
+      [one, _two, _three] = Callers.callers("ExFactor.Parser", "all_functions", 1)
+
+      assert one.caller_module == ExFactor.Callers
+      assert one.file == "lib/ex_factor/callers.ex"
+      assert one.line == 9
+    end
+
+    test "when the module is invalid" do
+      assert [] = Callers.callers("ExFactor.NotParser", "all_functions", 1)
+    end
+
+    test "when the function is invalid" do
+      assert [] = Callers.callers("ExFactor.Parser", "not_a_function", 1)
+    end
+
+    test "when the arity doesn't match" do
+      assert [] = Callers.callers("ExFactor.Parser", "all_functions", 7)
+    end
+  end
 end
