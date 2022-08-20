cd559875c5ed1f7cf7bebd0eec40968367dacba3
Christian Koch <ckoch@cars.com>
2022-08-19 21:50:59 -0500

    Tried Changex again, dropped it again

--- mix.exs
+++ mix.exs
@@ -48,7 +48,6 @@ defmodule ExFactor.MixProject do
 
   defp deps do
     [
-      {:changex, ">= 0.0.0", only: :dev, runtime: false, optional: true, app: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false, optional: true, app: false}
     ]
   end
